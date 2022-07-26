929a2e7f30603092779898dfa9a0de7420c52899
2782777036 <56435126+2782777036@users.noreply.github.com>
2022-07-26 12:13:41 +0800

    Delete tiktok directory

--- tiktok/chrome_plus/js/control.js
+++
@@ -1,9 +0,0 @@
-var start_uid = document.getElementById("uid_start");
-var start_action = document.getElementById("action_start");
-
-run=start_uid.addEventListener("click", function(){
-    for (j=0;j<document.getElementsByClassName("test_info").length;j++){
-        document.getElementsByClassName("test_info")[j].remove()
-    }
-    
-})
